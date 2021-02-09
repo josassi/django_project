f085a2c0ac6c9934b8a12bc6a71738ac5233e81c
Johan <johan.sassi@gmail.com>
2021-02-09 16:28:20 +0100

    Update admin interface

--- disquaire_project/disquaire_project/urls.py
+++ disquaire_project/disquaire_project/urls.py
@@ -20,9 +20,9 @@ from django.contrib import admin
 from store import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
     url(r'^store/', include(('store.urls', 'store'), namespace='store')),
-    url(r'^admin/', admin.site.urls)
+    url(r'^content/', admin.site.urls)
 ]
 
 if settings.DEBUG:
--- disquaire_project/store/forms.py
+++ disquaire_project/store/forms.py
@@ -7,11 +7,9 @@ from .models import Contact
 class ParagraphErrorList(ErrorList):
     def __str__(self):
         return self.as_divs()
-
     def as_divs(self):
         if not self: return ''
         return '<div class="errorlist">%s</div>' % ''.join(['<p class="small error">%s</p>' % e for e in self])
-
 
 class ContactForm(ModelForm):
     class Meta:
